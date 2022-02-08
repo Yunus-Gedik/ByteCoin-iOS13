cbd1d60d293cc95f0e96de08c761061078fd85c4
Yunus-Gedik <yunus-gedik@outlook.com>
2022-02-08 23:35:36 +0300

    feat: completed project

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -1,20 +1,57 @@
-//
-//  ViewController.swift
-//  ByteCoin
-//
-//  Created by Angela Yu on 11/09/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var amount: UILabel!
+    @IBOutlet var currencyLabel: UILabel!
+    @IBOutlet var picker: UIPickerView!
+    
+    var coinManager = CoinManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        
+        coinManager.delegate = self
+        picker.delegate = self
+        picker.dataSource = self
 
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        picker.delegate?.pickerView!(picker, didSelectRow: 0, inComponent: 0)
+    }
 
 }
 
+
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource{
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+    
+    
+    public func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        coinManager.fetchData(currencyRow: row)
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+    
+}
+
+extension ViewController: CoinManagerDelegate{
+    func didSuccess(coin: CoinData) {
+        DispatchQueue.main.async {
+            self.amount.text = String(format: "%.1f", coin.rate)
+            self.currencyLabel.text = coin.asset_id_quote
+        }
+    }
+    
+    func didFail(error: Error) {
+        print(error)
+    }
+}
--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -1,19 +1,49 @@
-//
-//  CoinManager.swift
-//  ByteCoin
-//
-//  Created by Angela Yu on 11/09/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import Foundation
 
-struct CoinManager {
-    
-    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
-    
-    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+protocol CoinManagerDelegate{
+    func didSuccess(coin: CoinData)
+    func didFail(error: Error)
+}
 
+struct CoinManager {
+
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "5ABCB1FE-AD3E-482C-B1CE-56F8AFC834CD"
+    let currencyArray = ["TRY","AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
+    var delegate: CoinManagerDelegate?
+    
+    func fetchData(currencyRow row: Int){
+        if let url = URL(string: "\(baseURL)/\(currencyArray[row])?apikey=\(apiKey)") {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    self.delegate!.didFail(error: error!)
+                    return
+                }
+                if let safeData = data {
+                    if let coin = self.parseJSON(safeData) {
+                        self.delegate!.didSuccess(coin: coin)
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    func parseJSON(_ data: Data)-> CoinData?{
+        let decoder = JSONDecoder()
+        
+        do {
+            let decodedData = try decoder.decode(CoinData.self, from: data)
+            return decodedData
+            
+        } catch {
+            delegate!.didFail(error: error)
+            return nil
+        }
+        
+    }
+    
     
 }
