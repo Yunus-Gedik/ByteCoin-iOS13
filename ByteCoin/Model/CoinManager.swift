cb3d5f793d5aae08c37e1478646c8ac7aac02649
Yunus-Gedik <yunus-gedik@outlook.com>
2022-04-14 16:25:16 +0300

    refactor: some not so important changes

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -7,7 +7,7 @@ protocol CoinManagerDelegate{
 
 struct CoinManager {
 
-    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    private let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
     let apiKey = "5ABCB1FE-AD3E-482C-B1CE-56F8AFC834CD"
     let currencyArray = ["TRY","AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
     
